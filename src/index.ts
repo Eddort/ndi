cdeb758029fc673cef58a0ca1009ce4736f2ca21
Eddort <webisforce@gmail.com>
2018-07-22 09:31:37 +0300

    supervisor is added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-class User {
-	private _name: string = "ssss";
-
-	public get name(): string {
-		return this._name;
-	}
-
-	public set name(n: string) {
-		this._name = n;
-	}
-}
-let tom: User = new User();
-console.log(tom._name, tom.name);
+console.log(1)
+process.exit(1)
